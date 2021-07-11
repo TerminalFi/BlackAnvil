1d3b00b7d34ede429a1132a1e0776fc1eca383da
TheSecEng <zachary.schulze@gmail.com>
2021-07-11 09:18:49 -0700

    temp site

--- source/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import IndexPageView, contact_us
-
-urlpatterns = [
-    path('', IndexPageView.as_view(), name='index'),
-    path('contact/', contact_us, name='contact'),
-]
